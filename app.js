4839d8153b7f224a6f780354cad2958d0c42e168
Jbrown95 <browningjustin95@gmail.com>
2018-11-20 23:54:21 -0500

    learning redirects in node

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var port = 4567
+var port = 3000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,5 +39,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () => console.log(`Server running on ${port}`))
+app.listen(port, function() {
+  console.log(`Server is running my man on ${port}`);
+});
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+  res.render('index');
 });
 
-router.get('/other_page', function(req, res, next) {
+router.get('/other_page', function(req, res, next){
   res.render('other_page');
 });
 
-router.post('/this_happens', function(req, res) {
-  res.render('other_page', {text: 'passing information'});
+router.post('/this_happens', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
