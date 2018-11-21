c133c313be702312aabc561737f5a90136f1ee4e
Jbrown95 <browningjustin95@gmail.com>
2018-11-20 23:03:15 -0500

    tested routes to pages

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var port = 4567
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,4 +39,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(port, () => console.log(`Server running on ${port}`))
 module.exports = app;
